37a394e8c9c3a3ddbb5f96cc900837bd3f3f4206
aishaapat <aishapatel0703@gmail.com>
2025-04-01 22:31:32 -0400

    added all the logic for the keyboard on the contacts view controller

--- MyContactList/ContactsViewController.swift
+++ MyContactList/ContactsViewController.swift
@@ -9,21 +9,51 @@ import UIKit
 
 class ContactsViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!  // Changed to UIScrollView
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        registerKeyBoardNotifications()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.unregisterKeyBoardNotifications()
     }
-    */
 
+    func registerKeyBoardNotifications() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardDidShow(notification:)),
+                                               name: UIResponder.keyboardDidShowNotification,
+                                               object: nil)
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillHide(notification:)),
+                                               name: UIResponder.keyboardWillHideNotification,
+                                               object: nil)
+    }
+    
+    func unregisterKeyBoardNotifications() {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    @objc func keyboardDidShow(notification: NSNotification) {
+        if let userInfo = notification.userInfo,
+           let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+            
+            let keyboardSize = keyboardFrame.cgRectValue.size
+            var contentInset = scrollView.contentInset
+            contentInset.bottom = keyboardSize.height
+            scrollView.contentInset = contentInset
+            scrollView.scrollIndicatorInsets = contentInset
+        }
+    }
+
+    @objc func keyboardWillHide(notification: NSNotification) {
+        var contentInset = scrollView.contentInset
+        contentInset.bottom = 0
+        scrollView.contentInset = contentInset
+        scrollView.scrollIndicatorInsets = UIEdgeInsets.zero
+    }
 }
+
