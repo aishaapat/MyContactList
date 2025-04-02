868eefa3a403bafa506571758a111d510bb7545e
aishaapat <aishapatel0703@gmail.com>
2025-04-01 23:49:36 -0400

    added method to change the edit

--- MyContactList/ContactsViewController.swift
+++ MyContactList/ContactsViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ContactsViewController: UIViewController {
-
+    @IBOutlet weak var txtName: UITextField!
+    @IBOutlet weak var txtCity: UITextField!
+    @IBOutlet weak var txtAddress: UITextField!
+    
+    @IBOutlet weak var sgmtEditMode: UISegmentedControl!
+    @IBOutlet weak var btnChange: UIButton!
+    @IBOutlet weak var lblBirthdaye: UILabel!
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var txtPhone: UITextField!
+    @IBOutlet weak var txtCell: UITextField!
+    @IBOutlet weak var txtZip: UITextField!
+    @IBOutlet weak var txtState: UITextField!
+    @IBAction func changeEditMode(_ sender: Any) {
+    }
+    
     @IBOutlet weak var scrollView: UIScrollView!  // Changed to UIScrollView
 
     override func viewDidLoad() {
         super.viewDidLoad()
         registerKeyBoardNotifications()
+        sgmtEditMode.addTarget(self, action: #selector(changeEditMode(sender:)), for: .valueChanged)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -55,5 +70,31 @@ class ContactsViewController: UIViewController {
         scrollView.contentInset = contentInset
         scrollView.scrollIndicatorInsets = UIEdgeInsets.zero
     }
+    
+    // adding button stuff for birthday now
+    override func didReceiveMemoryWarning(){
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func changeEditMode(sender: Any) {
+        let textFields: [UITextField] =
+        [txtZip, txtCell, txtCity, txtName, txtEmail, txtPhone, txtState, txtAddress]
+
+        if sgmtEditMode.selectedSegmentIndex == 0 {
+            for textField in textFields {
+                textField.isEnabled = false
+                textField.borderStyle = .none
+            }
+        }
+        else if sgmtEditMode.selectedSegmentIndex == 1 {
+            for textField in textFields {
+                textField.isEnabled = true
+                textField.borderStyle = .roundedRect
+            }
+            btnChange.isHidden = false
+        }
+    }
+
+
 }
 
