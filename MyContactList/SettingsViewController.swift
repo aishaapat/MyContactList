2d227690e986bd3b3ae1ccbedd747094f00e901a
aishaapat <aishapatel0703@gmail.com>
2025-04-14 22:43:46 -0400

    added picker values

--- MyContactList/SettingsViewController.swift
+++ MyContactList/SettingsViewController.swift
@@ -7,23 +7,36 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+
+    @IBOutlet weak var swAscending: UISwitch!
+    @IBOutlet weak var pckSortField: UIPickerView!
+
+    let sortOrderItems: [String] = ["ContactName", "City", "Birthday"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        pckSortField.dataSource = self
+        pckSortField.delegate = self
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - UIPickerViewDataSource
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
     }
-    */
 
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return sortOrderItems.count
+    }
+
+    // MARK: - UIPickerViewDelegate
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return sortOrderItems[row]
+    }
+
+    // Optional: Handle picker selection
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let selectedItem = sortOrderItems[row]
+        print("Selected sort field: \(selectedItem)")
+    }
 }
