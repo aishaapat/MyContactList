d25e48dc808ad4358487f22d5fcbf6b3e670e99a
aishaapat <aishapatel0703@gmail.com>
2025-04-16 14:26:58 -0400

    added button to add new contact

--- MyContactList/ContactsTableViewController.swift
+++ MyContactList/ContactsTableViewController.swift
@@ -6,29 +6,53 @@
 //
 
 import UIKit
-
+import CoreData
 class ContactsTableViewController: UITableViewController {
-
+   // let contacts = ["Jim", "John", "Dana", "Rosie", "Justin", "Jeremy", "Sarah", "Matt", "Joe", "Donald", "Jeff"]
+    var contacts: [NSManagedObject] = []
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        loadDataFromDatabase()
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    func loadDataFromDatabase() {
+        let context = appDelegate.persistentContainer.viewContext
+        let request = NSFetchRequest<NSManagedObject>(entityName: "Contact")
+        do {
+            contacts = try context.fetch(request)
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+    }
+
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+   
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return contacts.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ContactCell", for: indexPath)
+        let contact = contacts[indexPath.row] as? Contact
+        cell.textLabel?.text = contact?.contactName
+        cell.detailTextLabel?.text = contact?.city
+        return cell
     }
 
     /*
