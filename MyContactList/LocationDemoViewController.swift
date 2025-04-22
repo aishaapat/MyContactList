b766fa310bd030624b81b1bcde5ec50fed79d710
aishaapat <aishapatel0703@gmail.com>
2025-04-22 17:04:54 -0400

    Changes

--- MyContactList/LocationDemoViewController.swift
+++ MyContactList/LocationDemoViewController.swift
@@ -6,24 +6,72 @@
 //
 
 import UIKit
+import CoreLocation
 
-class LocationDemoViewController: UIViewController {
-
+class LocationDemoViewController:
+    UIViewController {
+    @IBOutlet weak var txtStreet: UITextField!
+    @IBOutlet weak var txtCity: UITextField!
+    @IBOutlet weak var lblLocationAccuracy: UILabel!
+    
+    @IBOutlet weak var txtState: UITextField!
+    @IBOutlet weak var lblLatitude: UILabel!
+    @IBOutlet weak var lblLongitude: UILabel!
+    @IBOutlet weak var lblHeading: UILabel!
+    @IBOutlet weak var lblHeadingAccuracy: UILabel!
+    
+    @IBOutlet weak var lblAltitude: UILabel!
+    @IBOutlet weak var lblAltitudeAccuracy: UILabel!
     override func viewDidLoad() {
+        lblHeading.text = ""
+        lblAltitude.text = ""
+        lblLatitude.text = ""
+        lblLongitude.text = ""
+        lblHeadingAccuracy.text = ""
+        lblAltitudeAccuracy.text = ""
+        lblLocationAccuracy.text = ""
+        
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
+    lazy var geoCoder = CLGeocoder()
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func addressToCoordinates(_ sender: Any) {
+        let address = "\(txtStreet.text!), \(txtCity.text!), \(txtState.text!)"
+        geoCoder.geocodeAddressString(address) { (placemarks, error) in
+            self.processAddressResponse(withPlacemarks: placemarks, error: error)
+        }
+        
     }
-    */
+    private func processAddressResponse(withPlacemarks placemarks: [CLPlacemark]?, error: Error?) {
+        if let error = error {
+            print("Geocode Error: \(error)")
+        }
+        else {
+            var bestMatch: CLLocation?
+            if let placemarks = placemarks, placemarks.count > 0 {
+                bestMatch = placemarks.first?.location
+            }
+            if let coordinate = bestMatch?.coordinate {
+                lblLatitude.text = String (format: "%g\u{00B0}", coordinate.latitude)
+                lblLongitude.text = String (format: "%g\u{00B0}", coordinate.longitude)
+            }
+            else {
+                print("Didn't find any matching locations")
+            }
+        }
+    }
+    
+
+    @IBAction func deviceCoordinates(_ sender: Any)
+    {
+        
+    }
+    
+
+   
 
 }
